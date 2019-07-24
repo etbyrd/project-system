cac8d3ec2cad0288ab7a2172db3dd533ca7c17a6
Ethan Byrd <etbyrd@microsoft.com>
2019-07-24 14:41:32 -0700

    final working version

--- src/Microsoft.VisualStudio.Editors/PropPages/MultiTargetingDialog.Designer.vb
+++ src/Microsoft.VisualStudio.Editors/PropPages/MultiTargetingDialog.Designer.vb
@@ -3,8 +3,14 @@
 Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
     Partial Class MultiTargetingDialog
-        Friend WithEvents CommandLinePanel As System.Windows.Forms.Panel
         Friend WithEvents TFMSelector As System.Windows.Forms.ListView
+        Friend WithEvents CurrentTFMs As System.Windows.Forms.ListView
+        Friend WithEvents overarchingLayoutPanel As System.Windows.Forms.TableLayoutPanel
+        Friend WithEvents moveRightButton As System.Windows.Forms.Button
+        Friend WithEvents moveLeftButton As System.Windows.Forms.Button
+        Friend WithEvents moveUpButton As System.Windows.Forms.Button
+        Friend WithEvents moveDownButton As System.Windows.Forms.Button
+
         Private components As System.ComponentModel.IContainer
 
 
@@ -20,27 +26,70 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
         <System.Diagnostics.DebuggerNonUserCode()> Private Sub InitializeComponent()
             Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MultiTargetingDialog))
             Me.TFMSelector = New System.Windows.Forms.ListView()
-            Me.CommandLinePanel = New System.Windows.Forms.Panel()
+            Me.CurrentTFMs = New System.Windows.Forms.ListView()
+            Me.moveRightButton = New System.Windows.Forms.Button()
+            Me.moveLeftButton = New System.Windows.Forms.Button()
+            Me.moveUpButton = New System.Windows.Forms.Button()
+            Me.moveDownButton = New System.Windows.Forms.Button()
+            Me.overarchingLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+            Me.overarchingLayoutPanel.SuspendLayout()
             Me.SuspendLayout()
             '
             'TFMSelector
             '
+            Me.TFMSelector.Groups.AddRange(New System.Windows.Forms.ListViewGroup() {CType(resources.GetObject("TFMSelector.Groups"), System.Windows.Forms.ListViewGroup), CType(resources.GetObject("TFMSelector.Groups1"), System.Windows.Forms.ListViewGroup), CType(resources.GetObject("TFMSelector.Groups2"), System.Windows.Forms.ListViewGroup), CType(resources.GetObject("TFMSelector.Groups3"), System.Windows.Forms.ListViewGroup)})
             Me.TFMSelector.HideSelection = False
             resources.ApplyResources(Me.TFMSelector, "TFMSelector")
             Me.TFMSelector.Name = "TFMSelector"
+            Me.overarchingLayoutPanel.SetRowSpan(Me.TFMSelector, 4)
             Me.TFMSelector.UseCompatibleStateImageBehavior = False
             '
-            'CommandLinePanel
+            'CurrentTFMs
             '
-            resources.ApplyResources(Me.CommandLinePanel, "CommandLinePanel")
-            Me.CommandLinePanel.Name = "CommandLinePanel"
+            Me.CurrentTFMs.HideSelection = False
+            resources.ApplyResources(Me.CurrentTFMs, "CurrentTFMs")
+            Me.CurrentTFMs.Name = "CurrentTFMs"
+            Me.overarchingLayoutPanel.SetRowSpan(Me.CurrentTFMs, 4)
+            Me.CurrentTFMs.UseCompatibleStateImageBehavior = False
+            '
+            'moveRightButton
+            '
+            resources.ApplyResources(Me.moveRightButton, "moveRightButton")
+            Me.moveRightButton.Name = "moveRightButton"
+            '
+            'moveLeftButton
+            '
+            resources.ApplyResources(Me.moveLeftButton, "moveLeftButton")
+            Me.moveLeftButton.Name = "moveLeftButton"
+            '
+            'moveUpButton
+            '
+            resources.ApplyResources(Me.moveUpButton, "moveUpButton")
+            Me.moveUpButton.Name = "moveUpButton"
+            '
+            'moveDownButton
+            '
+            resources.ApplyResources(Me.moveDownButton, "moveDownButton")
+            Me.moveDownButton.Name = "moveDownButton"
+            '
+            'overarchingLayoutPanel
+            '
+            Me.overarchingLayoutPanel.Controls.Add(Me.TFMSelector, 0, 0)
+            Me.overarchingLayoutPanel.Controls.Add(Me.CurrentTFMs, 2, 0)
+            Me.overarchingLayoutPanel.Controls.Add(Me.moveUpButton, 1, 0)
+            Me.overarchingLayoutPanel.Controls.Add(Me.moveDownButton, 1, 1)
+            Me.overarchingLayoutPanel.Controls.Add(Me.moveRightButton, 1, 2)
+            Me.overarchingLayoutPanel.Controls.Add(Me.moveLeftButton, 1, 3)
+            resources.ApplyResources(Me.overarchingLayoutPanel, "overarchingLayoutPanel")
+            Me.overarchingLayoutPanel.Name = "overarchingLayoutPanel"
             '
             'MultiTargetingDialog
             '
             resources.ApplyResources(Me, "$this")
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.Controls.Add(Me.TFMSelector)
+            Me.Controls.Add(Me.overarchingLayoutPanel)
             Me.Name = "MultiTargetingDialog"
+            Me.overarchingLayoutPanel.ResumeLayout(False)
             Me.ResumeLayout(False)
 
         End Sub
--- src/Microsoft.VisualStudio.Editors/PropPages/MultiTargetingDialog.vb
+++ src/Microsoft.VisualStudio.Editors/PropPages/MultiTargetingDialog.vb
@@ -7,7 +7,9 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
         'so that the event doesn't fire BeginInvoke before the window is fully created
         Private canHandleEvents As Boolean = False
-
+        'Starting Hash for empty
+        Private currentTFMsHash As Integer = -1
+        Private BeginningTFMsHash As Integer = -1
 
         Public Sub New()
             MyBase.New()
@@ -16,14 +18,21 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             InitializeComponent()
 
             'Add any initialization after the InitializeComponent() call
-            AddChecks()
+            InitTFMs()
 
             TFMSelector.View = View.Details
             TFMSelector.Columns.Add("Frameworks")
             TFMSelector.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
             TFMSelector.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
-            TFMSelector.CheckBoxes = True
+            TFMSelector.CheckBoxes = False
             TFMSelector.HeaderStyle = ColumnHeaderStyle.None
+
+            CurrentTFMs.View = View.Details
+            CurrentTFMs.Columns.Add("Frameworks")
+            CurrentTFMs.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent)
+            CurrentTFMs.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+            CurrentTFMs.CheckBoxes = False
+            CurrentTFMs.HeaderStyle = ColumnHeaderStyle.None
         End Sub
 
         Protected Overrides Sub PostInitPage()
@@ -31,21 +40,26 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             canHandleEvents = True
         End Sub
 
-        Private Sub AddChecks()
-
+        Private Sub InitTFMs()
             For Each supportedFramework As TargetFrameworkMoniker In ApplicationPropPageInternalBase.SupportedFrameworks
                 Dim item As ListViewItem = New ListViewItem()
                 item.Text = supportedFramework.ToString
-                item.Tag = supportedFramework
+                item.Tag = New FrameworkName(supportedFramework.Moniker)
                 TFMSelector.Items.Add(item)
+                'Move into the correct group
+                If String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETFramework") Then
+                    item.Group = TFMSelector.Groups.Item(1)
+                ElseIf String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETCoreApp") Then
+                    item.Group = TFMSelector.Groups.Item(2)
+                ElseIf String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETStandard") Then
+                    item.Group = TFMSelector.Groups.Item(3)
+                End If
             Next
-
         End Sub
 
         Private Delegate Sub ProcessItemCheck(ByRef listBoxObject As ListView)
 
-        Private Sub ProcessItemCheckSub(ByRef listBoxObject As ListView)
-            'SetTargetFrameworks()
+        Private Sub ProcessItemMovedSub(ByRef listBoxObject As ListView)
             IsDirty = True
         End Sub
 
@@ -53,9 +67,123 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
             'Once we implement checking the framework value, then we can implement only handling the event if it differs from what is in the project file 
             If canHandleEvents Then
                 Dim Objects As Object() = {TFMSelector}
-                BeginInvoke(New ProcessItemCheck(AddressOf ProcessItemCheckSub), Objects)
+                BeginInvoke(New ProcessItemCheck(AddressOf ProcessItemMovedSub), Objects)
             End If
         End Sub
+
+        Private Sub RightButton_Clicked(sender As Object, e As EventArgs) Handles moveRightButton.Click
+            Dim copyOfItems = TFMSelector.SelectedItems
+            For Each item As ListViewItem In copyOfItems
+                MoveFrameworkItem(item, True)
+            Next
+            currentTFMsHash = TFMHash(CurrentTFMs.Items)
+        End Sub
+
+        Private Sub LeftButton_Clicked(sender As Object, e As EventArgs) Handles moveLeftButton.Click
+            Dim copyOfItems = CurrentTFMs.SelectedItems
+            For Each item As ListViewItem In copyOfItems
+                MoveFrameworkItem(item, False)
+            Next
+            currentTFMsHash = TFMHash(CurrentTFMs.Items)
+        End Sub
+
+        Private Sub UpButton_Clicked(sender As Object, e As EventArgs) Handles moveUpButton.Click
+            Dim copyOfItems = CurrentTFMs.SelectedItems
+            If copyOfItems.Count <> 0 Then
+                Dim item = copyOfItems.Item(0)
+                Dim itemPreviousLocation = item.Index
+                If itemPreviousLocation <> 0 Then
+                    CurrentTFMs.Items.Remove(item)
+                    CurrentTFMs.Items.Insert(itemPreviousLocation - 1, item)
+                End If
+            End If
+            currentTFMsHash = TFMHash(CurrentTFMs.Items)
+            IsDirty = True
+        End Sub
+
+        Private Sub DownButton_Clicked(sender As Object, e As EventArgs) Handles moveDownButton.Click
+            Dim copyOfItems = CurrentTFMs.SelectedItems
+            If copyOfItems.Count <> 0 Then
+                Dim item = copyOfItems.Item(0)
+                Dim itemPreviousLocation = item.Index
+                If itemPreviousLocation <> CurrentTFMs.Items.Count - 1 Then
+                    CurrentTFMs.Items.Remove(item)
+                    CurrentTFMs.Items.Insert(itemPreviousLocation + 1, item)
+                End If
+            End If
+            currentTFMsHash = TFMHash(CurrentTFMs.Items)
+            IsDirty = True
+        End Sub
+
+        Private Sub MoveTFMToCorrectLocation(item As ListViewItem)
+            'Determine the Group First
+            Dim GroupNumber = -1
+            If String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETFramework") Then
+                GroupNumber = 0
+            ElseIf String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETCoreApp") Then
+                GroupNumber = 1
+            ElseIf String.Equals(CType(item.Tag, FrameworkName).Identifier, ".NETStandard") Then
+                GroupNumber = 2
+            End If
+
+            item.Group = TFMSelector.Groups.Item(GroupNumber)
+            Dim itemFrameworkName = CType(item.Tag, FrameworkName)
+            Dim locationFound = False
+            Dim LastLocation2 = 0
+
+            For Each tfm As ListViewItem In TFMSelector.Items
+                If CType(tfm.Tag, FrameworkName).Identifier = itemFrameworkName.Identifier Then
+                    If CType(tfm.Tag, FrameworkName).Version > itemFrameworkName.Version Then
+                        TFMSelector.Items.Insert(TFMSelector.Items.IndexOf(tfm), item)
+                        locationFound = True
+                        Exit For
+                    End If
+                    LastLocation2 = TFMSelector.Items.IndexOf(tfm)
+                End If
+                'LastLocation2 += 1
+
+            Next
+            'It must have been higher than all them, so just add it
+            If Not locationFound Then
+                TFMSelector.Items.Insert(LastLocation2 + 1, item)
+            End If
+        End Sub
+
+        Private Sub MoveFrameworkItem(item As ListViewItem, toRight As Boolean)
+            If toRight Then
+                TFMSelector.Items.Remove(item)
+                CurrentTFMs.Items.Add(item)
+            Else
+                CurrentTFMs.Items.Remove(item)
+                'This isn't working correctly at the moment either. I add them in at the right location, but they still display them out of order.
+                'The fix is to just clear the list and re-add them propbably. Dumb, but I can add it back if I need to. 
+                'MoveTFMToCorrectLocation(item)
+                item.Group = TFMSelector.Groups.Item(0)
+                TFMSelector.Items.Add(item)
+                'Insert the item back into the correct location in the list
+                'This isn't working correctly, won't add back in items that are the highest in their framework. e.g. 4.7.2 will never be added.
+                'For Each tfm As ListViewItem In TFMSelector.Items
+                '    Dim tfmMoniker As TargetFrameworkMoniker = CType(item.Tag, TargetFrameworkMoniker)
+                '    Dim nextMoniker As TargetFrameworkMoniker = CType(TFMSelector.Items.Item(tfm.Index).Tag, TargetFrameworkMoniker)
+                '    Dim frameworkName1 As New FrameworkName(tfmMoniker.Moniker)
+                '    Dim frameworkName2 As New FrameworkName(nextMoniker.Moniker)
+                '    If (frameworkName1.Identifier = frameworkName2.Identifier AndAlso frameworkName2.Version > frameworkName1.Version) Then
+                '        TFMSelector.Items.Insert(tfm.Index, item)
+                '        Exit For
+                '    End If
+                'Next
+            End If
+            IsDirty = True
+        End Sub
+
+
+        Private Function TFMHash(TFMCollection As ListView.ListViewItemCollection) As Integer
+            Dim hash As Integer = 5454
+            For Each item As ListViewItem In TFMCollection
+                hash = hash * item.GetHashCode + item.GetHashCode
+            Next
+            Return hash
+        End Function
 
         ' I will need to define the exact behavior here eventually. For now, it will just assume that there is a TargetFrameworks property and only handle 
         ' Standard, Net, and NetCore. Also, 
@@ -63,39 +191,39 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
         Protected Sub PopulateTargetFrameworks()
             Dim currentTargetFrameworks As String = CStr(GetCommonPropertyValue(GetPropertyDescriptor("TargetFrameworks")))
             Dim frameworkList As String() = currentTargetFrameworks.Split(CType(";", Char()))
+
             For Each frameworkItem As ListViewItem In TFMSelector.Items
-                'There is a much more elegant way of doing this 
-                Dim selectedFrameworkMoniker As TargetFrameworkMoniker = CType(frameworkItem.Tag, TargetFrameworkMoniker)
                 Dim found = False
                 For Each currentFramework As String In frameworkList
-                    Dim ProjectFileName As String = TFMToProjectFileName(selectedFrameworkMoniker)
+                    Dim ProjectFileName As String = TFMToProjectFileName(CType(frameworkItem.Tag, FrameworkName))
                     If String.Equals(currentFramework, ProjectFileName) Then
                         found = True
                     End If
                 Next
                 If found Then
-                    frameworkItem.Checked = True
-                Else
-                    frameworkItem.Checked = False
+                    MoveFrameworkItem(frameworkItem, True)
                 End If
             Next
+            BeginningTFMsHash = TFMHash(CurrentTFMs.Items)
         End Sub
 
         Protected Overrides Sub OnApplyComplete(applySuccessful As Boolean)
             MyBase.OnApplyComplete(applySuccessful)
-            SetTargetFrameworks()
+
+            If currentTFMsHash <> BeginningTFMsHash Then
+                SetTargetFrameworks()
+            End If
         End Sub
 
         Private Sub SetTargetFrameworks()
             Dim TargetFrameworksString As String = ""
 
-            If TFMSelector.CheckedItems.Count = 1 Then
-                TargetFrameworksString = TFMToProjectFileName(CType(TFMSelector.CheckedItems.Item(0).Tag, TargetFrameworkMoniker))
+            If CurrentTFMs.Items.Count = 1 Then
+                TargetFrameworksString = TFMToProjectFileName(CType(CurrentTFMs.Items.Item(0).Tag, FrameworkName))
                 'TargetFrameworksString = TFMSelector.CheckedItems.Item(0).ToString
             Else
-                For Each selectedFrameworkItem As ListViewItem In TFMSelector.CheckedItems
-                    Dim selectedFrameworkMoniker As TargetFrameworkMoniker = CType(selectedFrameworkItem.Tag, TargetFrameworkMoniker)
-                    TargetFrameworksString += TFMToProjectFileName(selectedFrameworkMoniker) + ";"
+                For Each selectedFrameworkItem As ListViewItem In CurrentTFMs.Items
+                    TargetFrameworksString += TFMToProjectFileName(CType(selectedFrameworkItem.Tag, FrameworkName)) + ";"
                 Next
 
                 TargetFrameworksString = TargetFrameworksString.TrimEnd(CChar(";"))
@@ -104,8 +232,7 @@ Namespace Microsoft.VisualStudio.Editors.PropertyPages
 
             SetCommonPropertyValue(GetPropertyDescriptor("TargetFrameworks"), TargetFrameworksString)
         End Sub
-        Private Shared Function TFMToProjectFileName(moniker As TargetFrameworkMoniker) As String
-            Dim frameworkName As New FrameworkName(moniker.Moniker)
+        Private Shared Function TFMToProjectFileName(frameworkName As FrameworkName) As String
 
             Dim isNetCoreApp = String.Compare(frameworkName.Identifier, ".NETCoreApp", StringComparison.Ordinal) = 0
             Dim isNetStandard = String.Compare(frameworkName.Identifier, ".NETStandard", StringComparison.Ordinal) = 0
